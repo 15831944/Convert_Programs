7ee1786c480ae530149ef216904318bff41b10b3
K. C. Juntunen <kcjuntunen@frontier.com>
2015-05-19 07:05:07 -0400

    Initial commit

---
+++ Convert Programs/ErrMsg.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Convert_Programs
+{
+    /// <summary>
+    /// Interaction logic for ErrMsg.xaml
+    /// </summary>
+    public partial class ErrMsg : Window
+    {
+        private Exception _ourException;
+
+        public ErrMsg(Exception e)
+        {
+            InitializeComponent();
+            this._ourException = e;
+            
+        }
+
+        /// <summary>
+        /// Fills the error textbox
+        /// </summary>
+        private void ShowErr()
+        {
+            System.Media.SystemSounds.Beep.Play();
+            this.Title = "Error in " + _ourException.TargetSite;
+            string msg = string.Empty;
+
+            msg += string.Format("HRESULT: {0:x}\r\nMessage: {1}\r\n\r\nStack Trace:\r\n{2}\r\n\r\n",
+                _ourException.HResult, _ourException.Message, _ourException.StackTrace);
+
+            if (_ourException.InnerException != null)
+            {
+                msg += CatchInnerExceptions(_ourException);
+            }
+
+            this.tbMsg.Text = msg;
+        }
+
+        /// <summary>
+        /// Takes an exception, pulls out its inner exception, and builds an error message
+        /// </summary>
+        /// <param name="ex">A System.Exception object</param>
+        /// <returns>Returns an nicely constructed error message in a string</returns>
+        private string CatchInnerExceptions(Exception ex)
+        {
+            string msg = string.Empty;
+            Exception inEx = ex.InnerException;
+            msg += "Inner exception:\r\n";
+            msg += string.Format("HRESULT: {0:x}\r\nMessage: {1}\r\n\r\nStack Trace:\r\n{2}\r\n\r\n",
+                inEx.HResult, inEx.Message, inEx.StackTrace);
+
+            //Recursive! Watch out!
+            if (inEx.InnerException != null)
+                msg += CatchInnerExceptions(inEx);
+
+            return msg;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            string mailToLink = "mailto:" + Properties.Settings.Default.devEmail +
+                "?subject=" +
+                Properties.Settings.Default.errMailSub +
+                "&body=" + tbMsg.Text;
+            System.Diagnostics.Process.Start(mailToLink.Replace("\r\n", "%0A"));
+            this.Close();
+        }
+    }
+}
---
+++ Convert Programs/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Configuration;
+
+namespace Convert_Programs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Top = Properties.Settings.Default.Top;
+            this.Left = Properties.Settings.Default.Left;
+            this.fillCombobox();
+        }
+
+        private void fillCombobox()
+        {
+            System.Collections.Specialized.StringCollection pc = Properties.Settings.Default.machines;
+            foreach (string m in pc)
+            {
+                try
+                {
+                    this.cbxMachines.Items.Add(m);
+                }
+                catch (ArgumentNullException ane)
+                {
+                    ErrMsg em = new ErrMsg(ane);
+                    em.ShowDialog();
+                }
+                catch (SystemException se)
+                {
+                    ErrMsg em = new ErrMsg(se);
+                    em.ShowDialog();
+                }
+            }
+        }
+
+        private void ConvertProgram(string fileName, string selection)
+        {
+            SettingsPropertyCollection pc = Properties.Settings.Default.Properties;
+            FileInfo fi = new FileInfo(fileName);
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader sr = fi.OpenText())
+            {
+                string text = string.Empty;
+                try
+                {
+                    text = sr.ReadToEnd();
+                }
+                catch (OutOfMemoryException oome)
+                {
+                    ErrMsg em = new ErrMsg(oome);
+                    em.ShowDialog();
+                }
+                catch (IOException ioe)
+                {
+                    ErrMsg em = new ErrMsg(ioe);
+                    em.ShowDialog();
+                }
+
+                sb.Append(text);
+
+                System.Collections.Specialized.StringCollection sc = Properties.Settings.Default.machines;
+                foreach (string m in sc)
+                {
+                    SettingsProperty sp = pc[m];
+                    SettingsProperty target = pc[selection];
+                    System.Xml.Serialization.XmlSerializer xs = new System.Xml.Serialization.XmlSerializer(typeof(string[]));
+
+                    string[] sps = (string[])xs.Deserialize(new System.Xml.XmlTextReader(sp.DefaultValue.ToString(), System.Xml.XmlNodeType.Element, null));
+                    string[] tgts = (string[])xs.Deserialize(new System.Xml.XmlTextReader(target.DefaultValue.ToString(), System.Xml.XmlNodeType.Element, null));
+
+                    sb.Replace(sps[0], tgts[0]);
+                    sb.Replace(sps[1], tgts[1]);
+                }
+            }
+            this.SaveFile(fi, sb);
+        }
+
+        private void SaveFile(FileInfo file, StringBuilder sb)
+        {
+            using (StreamWriter sw = new StreamWriter(file.FullName, false))
+            {
+                try
+                {
+                    sw.Write(sb.ToString());
+                }
+                catch (ObjectDisposedException ode)
+                {
+                    ErrMsg em = new ErrMsg(ode);
+                    em.ShowDialog();
+                }
+                catch (NotSupportedException nse)
+                {
+                    ErrMsg em = new ErrMsg(nse);
+                    em.ShowDialog();
+                }
+                catch (IOException ioe)
+                {
+                    ErrMsg em = new ErrMsg(ioe);
+                    em.ShowDialog();
+                }
+            }
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string item in this.lbBatch.Items)
+            {
+                this.ConvertProgram(item, this.cbxMachines.SelectedItem.ToString());
+            }
+        }
+
+        private void lbBatch_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".lax";
+            dlg.Filter = "DDX PowerCam LAX file|*.lax";
+            dlg.Multiselect = true;
+            dlg.InitialDirectory = @"Q:\";
+            if (Properties.Settings.Default.lastDir != null)
+            {
+                dlg.InitialDirectory = Properties.Settings.Default.lastDir;
+            }
+
+            Nullable<bool> res = dlg.ShowDialog();
+
+            if (res == true)
+            {
+                foreach (string item in dlg.FileNames)
+                {
+                    try
+                    {
+                        this.lbBatch.Items.Add(item);
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        ErrMsg em = new ErrMsg(ane);
+                        em.ShowDialog();
+                    }
+                    catch (SystemException se)
+                    {
+                        ErrMsg em = new ErrMsg(se);
+                        em.ShowDialog();
+                    }
+                    System.IO.FileInfo fi = new FileInfo(item);
+                    Properties.Settings.Default.lastDir = fi.DirectoryName;
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Top = this.Top;
+            Properties.Settings.Default.Left = this.Left;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
