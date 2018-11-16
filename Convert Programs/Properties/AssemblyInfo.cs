fce13f9761e72e7b749b409b4a1c09d55f795687
KC Juntunen <juntunenkc@lan.amstore.com>
2018-11-16 08:29:15 -0500

    Update connection string; → .NET v4.7

--- Convert Programs/Properties/AssemblyInfo.cs
+++ Convert Programs/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Convert Programs")]
+[assembly: AssemblyTitle("Amstore Convert Programs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Amstore Corp")]
-[assembly: AssemblyProduct("Convert Programs")]
-[assembly: AssemblyCopyright("Copyright © Amstore 2015-2016")]
+[assembly: AssemblyProduct("Amstore Convert Programs")]
+[assembly: AssemblyCopyright("Copyright © K. C. Juntunen 2015-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
